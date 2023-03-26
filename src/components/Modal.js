e26b872f529756495537f01cfdca442bc679d3e5
Sulic Octavian <octavius1992@gmail.com>
2023-03-26 20:32:33 +0300

    Add app screen-shot

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { getData, setHighScores } from '../firebase/config';
 
 function Modal({ seconds, minutes, newGame }) {
